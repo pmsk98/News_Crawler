a6561774766e23111430c3f5d9166fde50f3575d
pmsk98 <45275607+pmsk98@users.noreply.github.com>
2021-10-07 14:16:27 +0900

    크롤링파일 수정

--- samsung_news_crawling.py
+++ samsung_news_crawling.py
@@ -4,7 +4,39 @@ from datetime import datetime
 import requests
 import pandas as pd
 import re
+import time
+#날짜 정제화 함수
+def date_cleansing(test):
+    try:
+        #지난 뉴스
+        #머니투데이  10면1단  2018.11.05.  네이버뉴스   보내기  
+        pattern = '\d+.(\d+).(\d+).'  #정규표현식 
+    
+        r = re.compile(pattern)
+        match = r.search(test).group(0)  # 2018.11.05.
+        date_text.append(match)
+        
+    except AttributeError:
+        #최근 뉴스
+        #이데일리  1시간 전  네이버뉴스   보내기  
+        pattern = '\w* (\d\w*)'     #정규표현식 
+        
+        r = re.compile(pattern)
+        match = r.search(test).group(1)
+        #print(match)
+        date_text.append(match)
 
+
+#내용 정제화 함수 
+def contents_cleansing(contents):
+    first_cleansing_contents = re.sub('<dl>.*?</a> </div> </dd> <dd>', '', 
+                                      str(contents)).strip()  #앞에 필요없는 부분 제거
+    second_cleansing_contents = re.sub('<ul class="relation_lst">.*?</dd>', '', 
+                                       first_cleansing_contents).strip()#뒤에 필요없는 부분 제거 (새끼 기사)
+    third_cleansing_contents = re.sub('<.+?>', '', second_cleansing_contents).strip()
+    contents_text.append(third_cleansing_contents)
+    #print(contents_text)
+    
 
 start=1
 title_text=[]
@@ -14,10 +46,9 @@ date_text=[]
 contents_text=[]
 result={}
 
-
 while True:
     try:
-        url = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=0&photo=0&field=0&pd=3&ds=2020.01.01&de=2020.12.31&cluster_rank=47&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:from20200101to20201231,a:all&start={}'.format(start)
+        url = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sort=2&photo=0&field=0&pd=3&ds=2016.01.01&de=2020.12.31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:from20160101to20201231,a:all&start={}'.format(start)
         headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
           'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
         response = requests.get(url, headers=headers)
@@ -55,7 +86,10 @@ while True:
         
         
         start += 10
+        time.sleep(0.5)
+        print(start)
     
     except: # 오류발생시 몇 페이지까지 크롤링했는지 page를 확인하기 
         print(start)
         break
+
